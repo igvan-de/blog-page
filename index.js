5e71ff238f95027002c25267d537d650f479903c
igvan-de <vdsmissenigor@gmail.com>
2022-03-30 22:51:04 +0200

    created layout artwork, need to fix carousel issue and working on footer

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-function classToggle() {
-    const navs = document.querySelectorAll('.Navbar__Items')
-    
-    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+const gap = 20;
+
+const carousel = document.getElementById("carousel"),
+  content = document.getElementById("content"),
+  next = document.getElementById("next"),
+  prev = document.getElementById("prev");
+
+next.addEventListener("click", () => {
+  carousel.scrollBy(width + gap, 0);
+  // create statement to set carousel back to start point
+  if (carousel.scrollWidth !== 0) {
+    prev.style.display = "flex";
   }
-  
-  document.querySelector('.Navbar__Link-toggle')
-    .addEventListener('click', classToggle);
+  if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "none";
+  }
+});
+prev.addEventListener("click", () => {
+  carousel.scrollBy(-(width + gap), 0);
+  // create statement to set carousel back to end point
+  if (carousel.scrollLeft - width - gap <= 0) {
+    prev.style.display = "none";
+  }
+  if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "flex";
+  }
+});
+
+let width = carousel.offsetWidth;
+window.addEventListener("resize", e => (width = carousel.offsetWidth));
