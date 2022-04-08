9ad176ad23626fce39c007b3bc7fb2dfba7cad2f
igvan-de <vdsmissenigor@gmail.com>
2022-04-08 10:56:28 +0200

    working on documentary section

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function toggleMobileMenu(menu) {
   menu.classList.toggle('open');
 }
+
+// function to check if required fields in form are correct and filled in
+// in not then show which ones are required
+
+function formCheck(form) {
+  console.log('form' , form);
+}
+
+// let nameEl = document.getElementById("nameId");
+// let surnameEl = ;
+// let meilEl = ;
+// let 
+
+// nameEl.setAttribute("required", true);
